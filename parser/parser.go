219636b748dc329f49a690bda6ade581ccc8c4d6
Kyle Loveless <kjloveless321@gmail.com>
2024-05-04 12:33:47 -0500

    parse expressions in let and return statements

--- parser/parser.go
+++ parser/parser.go
@@ -363,9 +363,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
         return nil
     }
 
-    // TODO: We're skipping the expressions until we 
-    // encounter a semicolon
-    for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+
+    stmt.Value = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
@@ -377,8 +379,8 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
     p.nextToken()
 
-    // TODO: We're skipping the expressions until we 
-    // encounter a semicolon
+    stmt.ReturnValue = p.parseExpression(LOWEST)
+
     for !p.curTokenIs(token.SEMICOLON) {
         p.nextToken()
     }

384fa1be5a56b6b5ba665bd9c7ce4883787a9731
Kyle Loveless <kjloveless321@gmail.com>
2024-05-06 11:11:36 -0500

    eval call functions.

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+    "bytes"
+    "fmt"
+    "monkey/ast"
+    "strings"
+)
 
 type ObjectType string
 
@@ -10,6 +15,7 @@ const (
     NULL_OBJ            = "NULL"
     RETURN_VALUE_OBJ    = "RETURN_VALUE"
     ERROR_OBJ           = "ERROR"
+    FUNCTION_OBJ        = "FUNCTION"
 )
 
 type Object interface {
@@ -35,6 +41,12 @@ type Error struct {
     Message string
 }
 
+type Function struct {
+    Parameters  []*ast.Identifier
+    Body        *ast.BlockStatement
+    Env         *Environment
+}
+
 func (e *Error) Type() ObjectType   { return ERROR_OBJ }
 func (e *Error) Inspect() string    { return "ERROR: " + e.Message }
 
@@ -49,3 +61,22 @@ func (rv *ReturnValue) Inspect() string     { return rv.Value.Inspect() }
 
 func (n *Null) Type() ObjectType    { return NULL_OBJ }
 func (n *Null) Inspect() string     { return "null" }
+
+func (f *Function) Type() ObjectType    { return FUNCTION_OBJ }
+func (f *Function) Inspect() string     {
+    var out bytes.Buffer
+
+    params := []string{}
+    for _, p := range f.Parameters {
+        params = append(params, p.String())
+    }
+
+    out.WriteString("fn")
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ", "))
+    out.WriteString(") {\n")
+    out.WriteString(f.Body.String())
+    out.WriteString("\n}")
+
+    return out.String()
+}

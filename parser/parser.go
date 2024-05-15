f0eac1bf5972b1abafd1f36f8e37bdae6c88352d
Kyle Loveless <kjloveless321@gmail.com>
2024-05-15 07:17:49 -0500

    Fix bug in parseReturnStatement

--- parser/parser.go
+++ parser/parser.go
@@ -465,7 +465,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
     stmt.ReturnValue = p.parseExpression(LOWEST)
 
-    for !p.curTokenIs(token.SEMICOLON) {
+    for p.curTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
